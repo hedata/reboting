5f0f2831a24441639127851eba6574747173fcf7
hedata <erich@thrive.to>
2017-03-31 17:43:21 +0200

    some server stuff

--- server/routes/datasources.js
+++ server/routes/datasources.js
@@ -2,16 +2,13 @@ var mongoose = require('mongoose');
 var DataSources = mongoose.model('DataSources');
 
 module.exports.newDataSource = function(req, res) {
-
-  // if(!req.body.name || !req.body.email || !req.body.password) {
-  //   sendJSONresponse(res, 400, {
-  //     "message": "All fields required"
-  //   });
-  //   return;
-  // }
   console.log(req.body);
   console.log("adding new Data Source: ");
   var datasource = new DataSources(req.body);
+  //search for a user and if it doenst exist. add it
+
+  //talk to bot here
+
 
   datasource.save(function(err,newdatasource) {
     if(err)
