da7682bb36ad00d3a42d4800a07d5e780b3860f4
hedata <erich@thrive.to>
2017-04-21 14:09:18 +0200

    making plotly work

--- src/app/components/visual/visual.component.ts
+++ src/app/components/visual/visual.component.ts
@@ -26,6 +26,30 @@ export class VisualComponent implements OnInit {
   public widget: any;
   @ViewChild('notebookElement')
   notebookElement: ElementRef;
+  public code_string = `import plotly;
+plotly.offline.init_notebook_mode();
+import plotly.graph_objs as go;
+import pandas as pd;
+fbData = pd.read_csv("FB_insights.csv", encoding = "ISO-8859-1");
+dataShort = fbData.iloc[1:, 1:10];
+dataShort.columns = ['Laufzeit_like_total', 'Daily_likes', 'Daily_Dislikes', 'PeoplePerDay', 'PeoplePerWeek', 'PeoplePer28Days', 'ReachDaily', 'ReachWeekly', 'Reach28Days'];
+days = list(range(1,32));
+var1 = list(dataShort['Daily_likes']);
+var2 = list(dataShort['Daily_Dislikes']);
+trace0 = go.Scatter(x = days, y = var1, name = "Daily_likes", line = dict(color = ('rgb(205, 12, 24)'), width = 4));
+trace1 = go.Scatter(x = days, y = var2, name = "Daily_Dislikes", line = dict(color = ('rgb(22, 96, 167)'), width = 4));
+data = [trace0, trace1];
+layout = dict(title = '(Dis)Likes and People per Day', xaxis = dict(title = 'Days'), yaxis = dict(title = 'absolut'),);
+fig = dict(data = data, layout = layout);
+plotly.offline.iplot(fig);`;
+  public code_string1= `import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline  
+x = np.linspace(0, 3*np.pi, 500)
+plt.plot(x, np.sin(x**2))
+plt.title('A simple chirp')
+plt.show()`;
   ngOnInit(): void {
     console.log('after view Checked');
     // set rendermine
@@ -53,7 +77,7 @@ export class VisualComponent implements OnInit {
       // create the widget
       this.widget = new OutputAreaWidget({ rendermime, model });
       console.log(this.widget);
-      this.widget.execute('print("hello");a=1+1;print(a)', session.kernel).then(reply => {
+      this.widget.execute(this.code_string, session.kernel).then(reply => {
         console.log('got reply from kernel: ' + reply.content.status);
         session.shutdown().then(() => {
           console.log('Session shut down');
