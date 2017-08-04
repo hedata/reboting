38aeba5437ab01825e1cfa42ba30ef1ed5342bef
hedata <erichheil@gmail.com>
2017-08-04 11:23:06 +0200

    creating component for #REBOTING-44

--- src/app/components/intent/intent.component.ts
+++ src/app/components/intent/intent.component.ts
@@ -1,6 +1,23 @@
 import {Component, Input, NgZone, OnInit} from '@angular/core';
 import { DataService } from '../../services/data.service';
-import annyang from 'annyang';
+
+import {
+  Session
+} from '@jupyterlab/services';
+import {
+  CodeCellModel, CodeCellWidget
+} from 'jupyterlab/lib/cells';
+
+import {
+  RenderMime
+} from 'jupyterlab/lib/rendermime';
+
+import {
+  Kernel
+} from '@jupyterlab/services';
+
+
+declare var $: any ;
 
 @Component({
   selector: 'app-intent',
@@ -12,6 +29,7 @@ export class IntentComponent {
   showComponent: String;
   constructor(
     private dataService: DataService,
+    private _ngZone: NgZone
   ) {
     dataService.changeEmitted$.subscribe(
       data => {
@@ -20,11 +38,18 @@ export class IntentComponent {
           case 'botanswer':
             console.log('Visual Component reacting to change');
             const response = data.data;
+            if (response.bot_response.result.action === 'create_intent') {
+              console.log('Intent Compoenent creating cell');
+              this.onCreateCodeCell();
+            }
             break;
           default:
             // console.log('not me');
             // console.log(data);
         }
       });
+  }
+  onCreateCodeCell() {
+   console.log('on create code cell');
   }
 }
